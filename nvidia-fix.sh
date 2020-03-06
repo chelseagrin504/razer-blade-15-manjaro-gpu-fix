c4e9924ee27bd1af3fb7a31e7e12084b7c06d1f5
nickedyer <nickedyer@gmail.com>
2020-03-06 02:15:23 -0500

    nvidia driver fix script

---
+++ nvidia-fix.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo mhwd -i pci video-nvidia
+sudo pacman --noconfirm -S linux54-headers linux55-headers xorg-xrandr xf86-video-intel git
+sudo pacman --noconfirm -S acpi_call-dkms
+sudo modprobe acpi_call
+mkdir Programs
+cd ~/Programs
+git clone https://github.com/dglt1/optimus-switch-sddm.git
+cd ~/Programs/optimus-switch-sddm
+nano ~/Programs/optimus-switch-sddm/switch/nvidia/optimus.sh
+chmod +x install.sh
+sudo ./install.sh
+cd ~/Programs/optimus-switch-sddm/switch
+chmod +x set-intel.sh
+chmod +x set-nvidia.sh
+sudo ./set-intel.sh
