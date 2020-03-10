643f49429688895e0af1290bd81e90ab84233142
nickedyer <nickedyer@gmail.com>
2020-03-10 10:46:33 -0400

    bigger stupid

--- nvidia-fix.sh
+++ nvidia-fix.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+sudo systemctl stop sddm
+sudo mhwd -r pci video-linux
 sudo mhwd -i pci video-nvidia-440xx
 sudo pacman --noconfirm -S linux54-headers linux55-headers xorg-xrandr xf86-video-intel git
 sudo pacman --noconfirm -S acpi_call-dkms
 sudo modprobe acpi_call
-mkdir Programs
+mkdir ~/Programs
 cd ~/Programs
 git clone https://github.com/dglt1/optimus-switch-sddm.git
 cd ~/Programs/optimus-switch-sddm
